9d4c82b24e1fd809b28a3ad7c66336f8d5088f8c
tinytoots <wangchen2018n@163.com>
2021-06-05 21:07:59 -0400

    add response layout

--- keyboard-official/src/index.js
+++ keyboard-official/src/index.js
@@ -4,11 +4,25 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { Layout, Menu } from 'antd';
 import './stat.js';
+
+const { Header, Content, Footer, Sider } = Layout;
 
 ReactDOM.render(
   <BrowserRouter>
+  <Sider
+      breakpoint="lg"
+      collapsedWidth="0"
+      onBreakpoint={broken => {
+        console.log(broken);
+      }}
+      onCollapse={(collapsed, type) => {
+        console.log(collapsed, type);
+      }}
+    >
     <App />
+    </Sider>
   </BrowserRouter>
   ,
   document.getElementById('root')
